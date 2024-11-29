42e88ba1c0c92b753a5f6d9d9bceaa2214ff4a6f
rifal-ops <muhrifal6465@gmail.com>
2024-11-29 16:15:02 +0800

    Add files via upload

---
+++ muh.js
@@ -0,0 +1,2 @@
+var hasil = document.getElementById("hasil-output");
+hasil.innerHTML = "<h1>Biodata</h1><p>Nama : Muh Rifal</p> <p>Kelas XI RPL</p>";
---
+++ popup.js
@@ -0,0 +1,8 @@
+function showPopup() {
+    document.getElementById("popup").style.display = "block";
+  }
+  
+  function closePopup() {
+    document.getElementById("popup").style.display   
+   = "none";
+  }
